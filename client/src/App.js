1e8e2b007aa9445bfbf258dbdb85d2b3b3b6b3cb
nihalpandey4 <rockingnihal1305@gmail.com>
2020-12-09 23:04:11 +0530

    basic structure and functionality created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={(e)=>this.handleSubmit(e)}>
+            <input
+              type="text"
+              placeholder="Enter task"
+              value={this.state.currentItem.text}
+              onChange={(e) => {
+                this.updateCurrentItemText(e.target.value);
+              }}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+        <ListItem items={this.state.items} deleteItem = {this.deleteItem} updateItem={this.updateItem} />
+      </div>
+    );
+  };
 }
 
 export default App;
