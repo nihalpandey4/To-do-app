0bf77029975c051ca8ec792dbcbb7c64b15cc961
nihalpandey4 <rockingnihal1305@gmail.com>
2020-12-10 22:51:51 +0530

    mobile only version test

--- client/src/components/ToDoApp.js
+++ client/src/components/ToDoApp.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 import "./ToDoApp.css";
 import ListItem from "./ListItem";
@@ -20,42 +20,44 @@ class ToDoApp extends React.Component {
   }
 
   updateCurrentItemText = (value) => {
-    this.setState({ currentItem: { ...this.state.currentItem, text: value,key: Date.now() } });
+    this.setState({
+      currentItem: { ...this.state.currentItem, text: value, key: Date.now() },
+    });
   };
 
-  handleSubmit = async (e)=>{
+  handleSubmit = async (e) => {
     e.preventDefault();
     const temp = this.state.currentItem;
-    if(temp.text===""){
-      return ;
+    if (temp.text === "") {
+      return;
     }
     await this.setState({
-      items:[...this.state.items,temp],
-      currentItem:{text:"",key:""}
+      items: [...this.state.items, temp],
+      currentItem: { text: "", key: "" },
     });
-  }
+  };
 
-  deleteItem = (key)=>{
-    const filteredItems = this.state.items.filter(item=> item.key!==key);
-    this.setState({items:filteredItems});
-  }
+  deleteItem = (key) => {
+    const filteredItems = this.state.items.filter((item) => item.key !== key);
+    this.setState({ items: filteredItems });
+  };
 
-  updateItem = (key,value)=>{
+  updateItem = (key, value) => {
     const tempList = this.state.items;
-    tempList.map(item=>{
-      if(item.key === key){
+    tempList.map((item) => {
+      if (item.key === key) {
         item.text = value;
       }
       return item;
-    })
-    this.setState({items:tempList});
-  }
+    });
+    this.setState({ items: tempList });
+  };
 
   render = () => {
     return (
       <React.Fragment>
         <header>
-          <form id="to-do-form" onSubmit={(e)=>this.handleSubmit(e)}>
+          <form id="to-do-form" onSubmit={(e) => this.handleSubmit(e)}>
             <input
               type="text"
               placeholder="Enter task"
@@ -67,7 +69,13 @@ class ToDoApp extends React.Component {
             <button type="submit">Add</button>
           </form>
         </header>
-        <ListItem items={this.state.items} deleteItem = {this.deleteItem} updateItem={this.updateItem} />
+        <div className="items-list">
+          <ListItem
+            items={this.state.items}
+            deleteItem={this.deleteItem}
+            updateItem={this.updateItem}
+          />
+        </div>
       </React.Fragment>
     );
   };
